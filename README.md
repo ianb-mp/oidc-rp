78dc9ea1b51313a5e81cba765ce885b52dd216f4
Ian Bishop <151477169+ianb-mp@users.noreply.github.com>
2024-09-24 10:46:17 +1000

    Dev (#1)
    * workflow
    * tweaks
    * README

---
+++ README.md
@@ -0,0 +1,12 @@
+# OIDC test
+
+A POC showing how to handle a Github Actions OIDC ID token as the RP (relying party)
+
+As per [Github Docs](https://docs.github.com/en/actions/security-for-github-actions/security-hardening-your-deployments/configuring-openid-connect-in-cloud-providers#requesting-the-jwt-using-environment-variables) these steps are involved:
+
+- Using workflow action runtime token to request a JWT from Github representing the OIDC ID token
+- POST the JWT to our cloud provider's token endpoint
+- receive an access token from the cloud provider
+
+The `oidc-rp` Go binary runs on the cloud provider side and receives the Github ID yoken & verifies it.
+
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -10,8 +13,18 @@ import (
 	"golang.org/x/net/context"
 )
 
-const issuerURL = "https://token.actions.githubusercontent.com"
-const clientID = "foobar"
+const (
+	// issuerURL corresponds with `iss` field
+	issuerURL = "https://token.actions.githubusercontent.com"
+	// clientID corresponds with `aud` field, which will be set to the Github user/org
+	clientID = "https://github.com/ianb-mp"
+	// limit is the maximum size of the request body
+	limit = 1024 * 1024
+)
+
+type JWTRequest struct {
+	JWT string `json:"jwt"`
+}
 
 func main() {
 	// Define a flag for the listen port
@@ -32,22 +45,43 @@ func main() {
 
 	// Define the HTTP handler for the JWT endpoint
 	http.HandleFunc("/jwt", func(w http.ResponseWriter, r *http.Request) {
-		// Parse the JWT from the request body
-		rawIDToken := r.FormValue("jwt")
+
+		var req JWTRequest
+
+		// Decode the request body into the JWTRequest struct
+		err := json.NewDecoder(io.LimitReader(r.Body, limit)).Decode(&req)
+		if err != nil {
+			log.Printf("Failed to decode request body: %v", err)
+			http.Error(w, "Failed to decode request body", http.StatusBadRequest)
+			return
+		}
 
 		// Verify the JWT
-		idToken, err := verifier.Verify(ctx, rawIDToken)
+		idToken, err := verifier.Verify(ctx, req.JWT)
 		if err != nil {
+			log.Printf("Failed to verify JWT: %v", err)
 			http.Error(w, "Failed to verify JWT", http.StatusBadRequest)
 			return
 		}
 
-		// Print the subject of the JWT
-		fmt.Fprintf(w, "Subject: %s", idToken.Subject)
+		log.Printf("raw token: %s", req.JWT)
+		buf := bytes.NewBuffer(nil)
+		PrintJSON(buf, idToken)
+		log.Printf("JWT verified: %s", buf.String())
+
+		// check other claims like subject
+
+		// return access token
 	})
 
 	// Start the HTTP server
 	addr := fmt.Sprintf(":%d", *port)
 	log.Printf("Server listening on %s", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+// https://www.reddit.com/r/golang/comments/gritgv/comment/fs3gdtu/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+func PrintJSON(w io.Writer, obj interface{}) {
+	bytes, _ := json.MarshalIndent(obj, "", "  ")
+	fmt.Fprintf(w, "%s\n", string(bytes))
 }
